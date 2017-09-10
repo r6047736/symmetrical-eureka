e18b3ad10635ee889c1e645fff29073ec424bf93
Haodong Ruan <r6047736@email.arizona.edu>
2017-09-09 18:08:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable, Subscribable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {AuthService} from './auth.service';
+import {MembersComponent} from './members/members.component';
+import {TasksService} from './tasks.service';
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
+
 })
 export class AppComponent {
-  title = 'app';
+
+
+  items :FirebaseListObservable<any>;
+  user: firebase.User;
+  userName : string="";
+  userDep:string;
+  userRealName:string;
+  msgVal: string;
+
+
+  constructor(public af:AngularFireDatabase, private authService : AuthService){
+    this.items = af.list('/messages',{
+      query:{
+        limitToLast:5
+      }
+    })
+    this.user = authService.user;
+
+
+  }
+
+  login(){
+    this.authService.login();
+  }
+  logout(){
+    this.authService.logout();
+  }
+  changeName(){
+    if(!this.userName || !this.userDep || !this.userRealName)
+    {
+      alert("填写全部三项基础信息再提交")
+      return;
+    }
+
+    this.authService.createAccount({
+      name:this.userName,
+      dep:this.userDep,
+      realName:this.userRealName,
+      id: this.authService.user.uid
+    })
+  }
+
+  chatSend(message: string){
+    this.items.push({message:message, name: this.authService.user.displayName});
+    this.msgVal = '';
+    console.log(this.items);
+}
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+
+import {AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+
+
+
+import { AuthService } from "./auth.service";
+import { MemberdbService } from './memberdb.service';
+import { TasksService } from './tasks.service';
+
+import {HttpModule} from "@angular/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import { KeysPipe } from './keys.pipe';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCFVyZG3tV1AKr1pcYaWL9TduR7me7eRs",
+  authDomain: "acss-manage.firebaseapp.com",
+  databaseURL: "https://acss-manage.firebaseio.com",
+  projectId: "acss-manage",
+  storageBucket: "acss-manage.appspot.com",
+  messagingSenderId: "311428748565"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    KeysPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule
+
   ],
-  providers: [],
+  providers: [AuthService,MemberdbService,TasksService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
